28a2b3512ffe5060e1bdbc5c702347de09505610
Joshua Peddle <Joshuapeddle128@gmail.com>
2022-10-06 11:15:30 -0230

    Wrap the usbSerial wait in DEBUG ifdef

--- src/main.h
+++ src/main.h
@@ -6,29 +6,27 @@
 
 #include <Servo.h>
 
-extern Servo servo_0;  // create servo object to control a servo
-extern Servo servo_1;  // create servo object to control a servo
-extern Servo servo_2;  // create servo object to control a servo
-extern Servo servo_3;  // create servo object to control a servo
-extern Servo servo_4;  // create servo object to control a servo
-extern Servo servo_5;  // create servo object to control a servo
+extern Servo servo_0; // create servo object to control a servo
+extern Servo servo_1; // create servo object to control a servo
+extern Servo servo_2; // create servo object to control a servo
+extern Servo servo_3; // create servo object to control a servo
+extern Servo servo_4; // create servo object to control a servo
+extern Servo servo_5; // create servo object to control a servo
 
 // Button defines
 #define DELAY_BETWEEN_CHECKS 100
 #define LONG_PRESS_MS 1000
-#define NO_PRESS 0     // No button press
-#define SHORT_PRESS 1  // On release < LONG_PRESS_MS
-#define LONG_PRESS 2   // On release > LONG_PRESS_MS
-#define HELD 4         // Held down
-#define PRESS 5        // On initial press
-
+#define NO_PRESS 0    // No button press
+#define SHORT_PRESS 1 // On release < LONG_PRESS_MS
+#define LONG_PRESS 2  // On release > LONG_PRESS_MS
+#define HELD 4        // Held down
+#define PRESS 5       // On initial press
 
 // IBUS receiver
-#define IBUSBM_NOTIMER 1 // no timer interrupt used
+#define IBUSBM_NOTIMER 1              // no timer interrupt used
 #define TIME_BETWEEN_IBUS_PRINTS 1000 // How long to wait before debugging the IBUS receiver
 #include <IBusBM.h>
 extern IBusBM IBus;
-
 
 // 9 DOF SENSOR and AHRS
 #define ADAFRUIT_SENSOR_CALIBRATION_USE_EEPROM 1
@@ -43,7 +41,8 @@ extern Adafruit_LSM9DS1 lsm9ds1;
 
 extern Adafruit_Sensor_Calibration_EEPROM cal;
 
-struct Ibus_data{
+struct Ibus_data
+{
     uint16_t c_1 = 0;
     uint16_t c_2 = 0;
     uint16_t c_3 = 0;
@@ -51,6 +50,7 @@ struct Ibus_data{
     uint16_t c_5 = 0;
     uint16_t c_6 = 0;
 };
+
 void setup();
 
 void loop();
