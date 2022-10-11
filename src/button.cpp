f3aa2242e7430604655c04e3b364c304fe2429f7
Joshua Peddle <Joshuapeddle128@gmail.com>
2022-10-11 17:53:15 -0230

    narrow releases scope

--- src/button.cpp
+++ src/button.cpp
@@ -1,18 +1,18 @@
 #include "main.h"
 
 
-unsigned long first_press = 0;
-unsigned long release=0;
-bool bootsed_pressed;
+unsigned long initial_press = 0;
 
-unsigned long last_check=0;
+bool bootsed_pressed = false; 
+
+unsigned long last_check=0; // When the last actual check of the buttons physical states was
 
 
-int last_button_state = 0;
+int last_button_state = 0;  // The last physical state of the button. Reported until last_check is exceeded and a new reading is taken.
 /// @brief  Check the button state
 /// @return NO_PRESS = no press, SHORT_PRESS = short press, LONG_PRESS = long press (> LONG_PRESS_MS), HELD = held, PRESS = press
 int check_button(){
-    
+
     if (millis() < last_check + DELAY_BETWEEN_CHECKS){
         return last_button_state;
     }
@@ -27,15 +27,15 @@ int check_button(){
     }
     if (BOOTSEL && !bootsed_pressed){
         bootsed_pressed=true;
-        first_press=last_check;
+        initial_press=last_check;
         debug("PRESS");
         last_button_state = PRESS;
         return PRESS;
     }
     if (!BOOTSEL && bootsed_pressed){
         bootsed_pressed=false;
-        release=last_check;
-        if (release-first_press>LONG_PRESS_MS){
+        unsigned long release=last_check;
+        if (release-initial_press>LONG_PRESS_MS){
             debug("Long press");
             last_button_state = LONG_PRESS;
             return LONG_PRESS;
