e1049c5df019c27d42ec7651b81d8abe198dfcfa
Joshua Peddle <Joshuapeddle128@gmail.com>
2023-01-04 21:10:40 -0330

    Adjust filter parameters

--- src/main.h
+++ src/main.h
@@ -10,13 +10,17 @@
 #include "button.h"
 
 
-#define DEBUG
+//#define DEBUG
 #define DEBUG_TIME // used for debugging time for loop
 
 
 // 9 DOF SENSOR and AHRS
+
 #define ADAFRUIT_SENSOR_CALIBRATION_USE_EEPROM 1
-#define FILTER_UPDATE_RATE_HZ 100
+#define FILTER_UPDATE_RATE_HZ 500  //hz
+#define FILTER_BETA 10.0f //ms
+#define PRINT_EVERY_N_UPDATES 50
+
 #include <Adafruit_Sensor_Calibration.h>
 #include <Adafruit_AHRS.h>
 #include <Adafruit_LSM9DS1.h>
