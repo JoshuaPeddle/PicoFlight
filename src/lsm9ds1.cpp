752e872e1031ee21436ca3907ee37c7e27f19395
Joshua Peddle <Joshuapeddle128@gmail.com>
2023-01-04 21:11:51 -0330

    Change file to use debug function

--- src/lsm9ds1.cpp
+++ src/lsm9ds1.cpp
@@ -8,6 +8,9 @@ bool init_sensors(void)
   {
     return false;
   }
+
+
+  
   delay(100);
   accelerometer = &lsm9ds1.getAccel();
   gyroscope = &lsm9ds1.getGyro();
@@ -19,34 +22,34 @@ bool init_sensors(void)
 void setup_sensors(void)
 {
   // set lowest range
-  lsm9ds1.setupAccel(lsm9ds1.LSM9DS1_ACCELRANGE_2G);
-  lsm9ds1.setupMag(lsm9ds1.LSM9DS1_MAGGAIN_4GAUSS);
-  lsm9ds1.setupGyro(lsm9ds1.LSM9DS1_GYROSCALE_245DPS);
+  lsm9ds1.setupAccel(lsm9ds1.LSM9DS1_ACCELRANGE_8G);
+  lsm9ds1.setupMag(lsm9ds1.LSM9DS1_MAGGAIN_16GAUSS);
+  lsm9ds1.setupGyro(lsm9ds1.LSM9DS1_GYROSCALE_2000DPS);
 }
 
 void start_calibration_engine()
 {
-  Serial.println("Calibration filesys test");
+  debug("Calibration filesys test");
   if (!cal.begin())
   {
-    Serial.println("Failed to initialize calibration helper");
+    debug("Failed to initialize calibration helper");
     while (1)
       yield();
   }
   if (!cal.loadCalibration())
   {
-    Serial.println("No calibration loaded/found... will start calibration");
+    debug("No calibration loaded/found... will start calibration");
     calibrate();
   }
   else
   {
-    Serial.println("Loaded existing calibration");
+    debug("Loaded existing calibration");
   }
 }
 
 void calibrate()
 {
-  Serial.println("**WARNING** Fake calibration");
+  debug("**WARNING** Fake calibration");
   cal.accel_zerog[0] = 0;
   cal.accel_zerog[1] = 0;
   cal.accel_zerog[2] = 0;
@@ -72,11 +75,11 @@ void calibrate()
   cal.mag_softiron[8] = 0;
   if (!cal.saveCalibration())
   {
-    Serial.println("**WARNING** Couldn't save calibration");
+    debug("**WARNING** Couldn't save calibration");
   }
   else
   {
-    Serial.println("Wrote calibration");
+    debug("Wrote calibration");
   }
   cal.printSavedCalibration();
 }
