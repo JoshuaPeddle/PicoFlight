a7261a469aba4b4dcceffe80a51c7cb004e9de8c
Joshua Peddle <Joshuapeddle128@gmail.com>
2023-01-06 23:03:17 -0330

    Re-arrange header functions

--- src/main.h
+++ src/main.h
@@ -8,22 +8,12 @@
 #include "ibus.h"
 #include "esc.h"
 #include "button.h"
+#include "lsm9ds1.h"
 
 
-//#define DEBUG
+#define DEBUG
 #define DEBUG_TIME // used for debugging time for loop
 
-
-// 9 DOF SENSOR and AHRS
-
-#define ADAFRUIT_SENSOR_CALIBRATION_USE_EEPROM 1
-#define FILTER_UPDATE_RATE_HZ 500  //hz
-#define FILTER_BETA 10.0f //ms
-#define PRINT_EVERY_N_UPDATES 50
-
-#include <Adafruit_Sensor_Calibration.h>
-#include <Adafruit_AHRS.h>
-#include <Adafruit_LSM9DS1.h>
 
 // GPS
 #include <GPSport.h>
@@ -34,29 +24,19 @@
 
 extern Adafruit_Sensor *accelerometer, *gyroscope, *magnetometer;
 
-extern Adafruit_LSM9DS1 lsm9ds1;
+//extern Adafruit_LSM9DS1 lsm9ds1;
 
-extern Adafruit_Sensor_Calibration_EEPROM cal;
+//extern Adafruit_Sensor_Calibration_EEPROM cal;
 
-extern struct Ibus_data ibus_data;
+//extern struct Ibus_data ibus_data;
 
 
 void setup();
 
 void loop();
 
-void setup_sensors();
 
-bool init_sensors();
-
-bool readIbus();
-
-void calibrate();
-
-void start_calibration_engine();
-
-void say_hello();
+void handle_button_press(int press);
 
 
-void handle_ibus_update();
 
